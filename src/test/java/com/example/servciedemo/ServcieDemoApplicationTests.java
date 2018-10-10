5694c38d060bed518955a94b379313840fd32333
hanlipeng <hanlipeng@nibirutech.com>
2018-10-10 12:29:30 +0800

    first commit

---
+++ src/test/java/com/example/servciedemo/ServcieDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.servciedemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServcieDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
